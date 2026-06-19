26198cc1e94284c136028921993cf1d2020f2a78
Kulashekar0 <kulashekarinkollu@gmail.com>
2026-06-19 10:07:50 +0530

    Product page complted

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,11 @@
+function ProductList() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        Product Listing Page
+      </h1>
+    </div>
+  );
+}
+
+export default ProductList;
